c91740e395dc0c6c7bc3c8964db45a05436c4dfc
Adam Jonas <adamjonas25@gmail.com>
2021-02-18 11:18:35 +0000

    extract another helper

--- spec/feature/entering_birthday_spec.rb
+++ spec/feature/entering_birthday_spec.rb
@@ -2,16 +2,14 @@ feature 'entering birthday' do
   scenario 'on user birthday' do
     visit_and_enter_name
     fill_in :day, with: Time.now.day
-    select Time.now.strftime('%B'), from: :month
-    click_button 'Go!'
+    enter_month_click_go
     expect(page).to have_content 'Happy Birthday'
   end
 
   scenario 'not on user birthday' do
     visit_and_enter_name
     fill_in :day, with: (Time.now - (60 * 60 * 48)).day
-    select Time.now.strftime('%B'), from: :month
-    click_button 'Go!'
+    enter_month_click_go
     expect(page).to have_content 'Your birthday will be in'
   end
 end
