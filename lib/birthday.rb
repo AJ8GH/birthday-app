6637ef11e16eb6e9aa5dc420579b4e51a80601a0
Adam Jonas <adamjonas25@gmail.com>
2021-02-19 08:52:38 +0000

    change method name time to year day

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,20 +14,20 @@ class Birthday
   attr_reader :birthday, :birth_month
 
   def today?(day, month)
-    date_to_day(day, month) == time_to_day(Time.now)
+    date_to_year_day(day, month) == time_to_year_day(Time.now)
   end
 
   def days_until_bday(day, month)
-    days = date_to_day(day, month) - time_to_day(Time.now)
+    days = date_to_year_day(day, month) - time_to_year_day(Time.now)
     days = 365 - days.abs if days.negative?
     days == 1 ? "#{days} day" : "#{days} days"
   end
 
-  def time_to_day(time)
+  def time_to_year_day(time)
     time.strftime('%j').to_i
   end
 
-  def date_to_day(day, month)
-    time_to_day(Time.parse("#{month}-#{day}"))
+  def date_to_year_day(day, month)
+    time_to_year_day(Time.parse("#{month}-#{day}"))
   end
 end
