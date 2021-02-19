37f6d3d5ff8622844c7413b5aca70c56f66c53e1
Adam Jonas <adamjonas25@gmail.com>
2021-02-19 12:30:03 +0000

    setup travis and coveralls

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'capybara'
+gem 'coveralls'
+gem 'rake'
 gem 'rspec'
 gem 'shotgun'
 gem 'sinatra'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.wear!
+
 require_relative '../app'
 require_relative 'feature/web_helpers'
 require          'capybara'
